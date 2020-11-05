28b9b1d8a4f062cca1e257cb29949251494eb8d0
Cloudy <cloud.kira@gmail.com>
2020-11-05 15:31:05 +0100

    Format time

--- Rhino_JS/src/test_js.js
+++ Rhino_JS/src/test_js.js
@@ -1,3 +1,8 @@
+
+function time(ms) {
+    return new Date(ms).toISOString().slice(11, -1);
+}
+
 function A(v) {
     return {value: () => v};
 }
@@ -10,24 +15,24 @@ const ELEMS_PER_ARRAY = 1000000;
 const MAX_VALUE = 100;
 const NUMBER_OF_TEST = 1000;
 
-let now_init = Date.now()
+let now = Date.now()
 const INT_ARRAY = new Array(ELEMS_PER_ARRAY).fill(null).map((_,i)=>Math.floor(Math.random()*MAX_VALUE));
-let then_init = Date.now()
-let delta = then_init-now_init
+let then = Date.now()
+let delta = then-now
 
-print("initialization of int_array done in "+ delta + " milliseconds ...");
+print("initialization of int_array done in "+ time(delta) + " milliseconds ...");
 
-let now_obj_init = Date.now()
-const OBJ_ARRAY = new Array(ELEMS_PER_ARRAY).fill(null).map((_,i)=>  {//new I(Math.floor(Math.random()*MAX_VALUE)));
+now = Date.now()
+const OBJ_ARRAY = new Array(ELEMS_PER_ARRAY).fill(null).map((_,i)=>  {
     if(i%2===0) {
         return new A(Math.floor(Math.random()*MAX_VALUE))
     } else {
         return new B(Math.floor(Math.random()*MAX_VALUE))
     }
 });
-let then_obj_init = Date.now()
-let delta_obj = then_obj_init - now_obj_init
-print("initialization of obj_array done in "+ delta_obj + " milliseconds ...");
+then = Date.now()
+delta = then - now
+print("initialization of obj_array done in "+ time(delta) + " milliseconds ...")
 
 function int_test(arr) {
     let sum = 0;
@@ -47,15 +52,25 @@ function obj_test(arr) {
 
 function launch_bench(func, arr) {
     const tests = Array();
+    let now, then;
     for(let j = 0; j < NUMBER_OF_TEST; ++j) {
-        const now = Date.now();
+        now = Date.now();
         func(arr);
-        const then = Date.now();
+        then = Date.now();
         tests[j] = then - now;
     }
-    print("test results with " + tests.length + " tests and " + arr.length + " elements:\n\n" + tests);
+    print("Performed " + tests.length + " tests and " + arr.length + " elements.");
     print("average result = " + tests.reduce((a,b) => a+b) / tests.length + " milliseconds");
 }
 
+now = Date.now()
 launch_bench(int_test, INT_ARRAY);
+then = Date.now()
+delta = then-now
+print("Total int_array benchmark time = " + time(delta))
+
+now = Date.now()
 launch_bench(obj_test, OBJ_ARRAY);
+then = Date.now()
+delta = then-now
+print("Total obj_array benchmark time = " + time(delta))
