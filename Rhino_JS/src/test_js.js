297e67ef6be0c0a5be3a5b3b26edbdd59b36fa91
Cloudy <cloud.kira@gmail.com>
2020-11-05 14:58:35 +0100

    Fixed Benchmark test on test_js.js

--- Rhino_JS/src/test_js.js
+++ Rhino_JS/src/test_js.js
@@ -1,42 +1,61 @@
-   function I(v) {
-           return {value: () => v};
-       }
+function A(v) {
+    return {value: () => v};
+}
 
-    const ELEMS_PER_ARRAY = 1000000;
-    const MAX_VALUE = 100;
-    const NUMBER_OF_TEST = 1000;
+function B(v) {
+    return {value: () => v};
+}
 
-    const INT_ARRAY = new Array(ELEMS_PER_ARRAY).fill(null).map((_,i)=>Math.floor(Math.random()*MAX_VALUE));
-    const OBJ_ARRAY = new Array(ELEMS_PER_ARRAY).fill(null).map((_,i)=>new I(Math.floor(Math.random()*MAX_VALUE)));
+const ELEMS_PER_ARRAY = 1000000;
+const MAX_VALUE = 100;
+const NUMBER_OF_TEST = 1000;
 
-    function int_test(arr) {
-        let sum = 0;
-        for(let i = 0; i < arr.length; ++i) {
-            sum += arr[i];
-        }
-        return sum;
+let now_init = Date.now()
+const INT_ARRAY = new Array(ELEMS_PER_ARRAY).fill(null).map((_,i)=>Math.floor(Math.random()*MAX_VALUE));
+let then_init = Date.now()
+let delta = then_init-now_init
+
+print("initialization of int_array done in "+ delta + " milliseconds ...");
+
+let now_obj_init = Date.now()
+const OBJ_ARRAY = new Array(ELEMS_PER_ARRAY).fill(null).map((_,i)=>  {//new I(Math.floor(Math.random()*MAX_VALUE)));
+    if(i%2===0) {
+        return new A(Math.floor(Math.random()*MAX_VALUE))
+    } else {
+        return new B(Math.floor(Math.random()*MAX_VALUE))
     }
+});
+let then_obj_init = Date.now()
+let delta_obj = then_obj_init - now_obj_init
+print("initialization of obj_array done in "+ delta_obj + " milliseconds ...");
 
-    function obj_test(arr) {
-        let sum = 0;
-        for(let i = 0; i < arr.length; ++i) {
-            sum += arr[i].value();
-        }
-        return sum;
+function int_test(arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; ++i) {
+        sum += arr[i];
     }
+    return sum;
+}
 
-    function launch_bench(func, arr) {
-        const tests = Array();
-        let j = 0;
-        for(let j = 0; j < NUMBER_OF_TEST; ++j) {
-            const now = Date.now();
-            func(arr);
-            const then = Date.now();
-            tests[j] = then - now;
-        }
-        console.log(tests);
-        console.log(tests.reduce((a,b) => a+b) / tests.length);
+function obj_test(arr) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; ++i) {
+        sum += arr[i].value();
     }
+    return sum;
+}
+
+function launch_bench(func, arr) {
+    const tests = Array();
+    for(let j = 0; j < NUMBER_OF_TEST; ++j) {
+        const now = Date.now();
+        func(arr);
+        const then = Date.now();
+        tests[j] = then - now;
+    }
+    print("test results with " + tests.length + " tests and " + arr.length + " elements:\n\n" + tests);
+    print("average result = " + tests.reduce((a,b) => a+b) / tests.length + " milliseconds");
+}
 
 launch_bench(int_test, INT_ARRAY);
 launch_bench(obj_test, OBJ_ARRAY);
